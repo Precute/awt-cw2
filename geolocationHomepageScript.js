d3ca11bd417a88a360f080933c557057d41b184c
Precious <precious_igbinosun@yahoo.co.uk>
2017-02-26 15:39:47 +0000

    Precious
    Precious

--- geolocationHomepageScript.js
+++ geolocationHomepageScript.js
@@ -8,6 +8,7 @@ var geocoder;
 var start = 1;
 var map;
 
+
 function watchPosition() {
     if (navigator.geolocation) {
         watchId = navigator.geolocation.watchPosition(successCallback, failPosition, {
@@ -152,11 +153,26 @@ function failPosition(error) {
                 //Create a new marker and info window
                 marker = new google.maps.Marker({
                     map: map,
+                    icon: {
+                        path: "M27.648 -41.399q0 -3.816 -2.7 -6.516t-6.516 -2.7 -6.516 2.7 -2.7 6.516 2.7 6.516 6.516 2.7 6.516 -2.7 2.7 -6.516zm9.216 0q0 3.924 -1.188 6.444l-13.104 27.864q-0.576 1.188 -1.71 1.872t-2.43 0.684 -2.43 -0.684 -1.674 -1.872l-13.14 -27.864q-1.188 -2.52 -1.188 -6.444 0 -7.632 5.4 -13.032t13.032 -5.4 13.032 5.4 5.4 13.032z",
+                        scale: 0.6,
+                        strokeWeight: 0.2,
+                        strokeColor: 'black',
+                        strokeOpacity: 1,
+                        fillColor: 'green',
+                        fillOpacity: 0.85,
+                    },
+                     mapTypeControlOptions: {
+                         mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+                            'styled_map']
+                  },
                     position: lat,
                     //Content is what will show up in the info window
                     content: addressDetails[0]
                 });
-                //Pushing the markers into an array so that it's easier to manage them
+                map.mapTypes.set('styled_map', styledMapType);
+                map.setMapTypeId('styled_map');
+                                //Pushing the markers into an array so that it's easier to manage them
                 markersArray.push(marker);
                 google.maps.event.addListener( marker, 'click', function () {
                     closeInfos();
